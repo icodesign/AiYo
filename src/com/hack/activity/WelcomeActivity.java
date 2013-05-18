b0ebd2a7fec789a736b56a5b03104e3d20751e7d
Boli-CS <libo4101001@gmail.com>
2013-05-18 16:36:30 +0800

    获取屏幕分辨率

--- src/com/hack/activity/WelcomeActivity.java
+++ src/com/hack/activity/WelcomeActivity.java
@@ -1,5 +1,11 @@
 package com.hack.activity;
 
+import com.hack.core.Hack;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+
 /**
  * 欢迎屏
  * @author lee
@@ -7,4 +13,17 @@ package com.hack.activity;
  */
 public class WelcomeActivity extends BaseActivity{
 	
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getScreenMatrix();
+    }
+
+	private void getScreenMatrix() {
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);   
+		Hack.SCREEN_HEIGHT_PIXEL = dm.heightPixels;
+		Hack.SCREEN_WIDTH_PIXEL = dm.widthPixels;
+	}
 }
