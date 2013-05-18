d6660687826b501753d42b0ce66d5d40ff68d853
Lee Wong <leimagnet@gmail.com>
2013-05-18 12:30:13 +0800

    init

---
+++ src/com/hack/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.hack.activity;
+
+import android.app.Activity;
+
+/**
+ * 所有Activity的父类，用来定义所有activity的共有行为，如makeToast
+ * @author lee
+ *
+ */
+public class BaseActivity extends Activity{
+
+}
---
+++ src/com/hack/activity/WelcomeActivity.java
@@ -0,0 +1,10 @@
+package com.hack.activity;
+
+/**
+ * 欢迎屏
+ * @author lee
+ *
+ */
+public class WelcomeActivity extends BaseActivity{
+	
+}
